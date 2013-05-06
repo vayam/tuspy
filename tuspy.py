88a58b5148d29b5c4ed23a18c5aa610f630fdd2c
Naren Venkataraman <naren@doorul.com>
2013-05-06 01:12:26 -0400

    Add retries/resume incase of server failure

--- tuspy.py
+++ tuspy.py
@@ -2,13 +2,21 @@ from optparse import OptionParser
 import os
 import sys
 import StringIO
+import time
+import traceback
 
 import requests
 import pycurl
 
 import config
 
-
+#exponential backoff
+initial_delay = 10 # 10 seconds
+max_attempts = 3
+def attempts():
+    for i in range(max_attempts):
+        yield i
+        time.sleep(initial_delay ** i)
 
 def die(msg, exit_code=0):
     print msg
@@ -29,10 +37,11 @@ def upload(location, filename, content_type, offset=0):
         c.setopt(pycurl.CUSTOMREQUEST, 'PATCH')
 
         f = open(filename, 'rb')
-        f.seek(offset)
+        if offset > 0: 
+            f.seek(offset)
         c.setopt(pycurl.READFUNCTION, f.read)
         filesize = os.path.getsize(filename)
-        c.setopt(pycurl.INFILESIZE, filesize)
+        c.setopt(pycurl.INFILESIZE, filesize - offset)
         c.setopt(pycurl.HTTPHEADER, ["Expect:", "Content-Type: %s" % content_type, "Offset: %d" % offset])
         c.perform()
 
@@ -70,14 +79,29 @@ if c.status_code != 201:
     die("create failure. reason: %s"  % c.reason)
 
 location = c.headers["Location"]
-
-offset = 0
 print location
-upload(location, filename, content_type, offset)
-h = requests.head(location)
-#print h.headers
-offset = int(h.headers["Offset"])
-print "Offset: ", offset
-if offset == os.path.getsize(filename):
-    die("upload success")
-die("upload failure")
+
+
+def get_offset(location):
+    h = requests.head(location)
+    offset = int(h.headers["Offset"])
+    print "Offset: ", offset
+    return offset 
+
+
+status = "upload failure"
+offset = 0
+for i in attempts():
+    try:
+        offset = get_offset(location)
+        if offset == filesize:
+            status = "upload success"
+            break
+        upload(location, filename, content_type, offset)
+        offset = get_offset(location)
+        if offset == filesize:
+            status = "upload success"
+            break
+    except:
+        traceback.print_exc()
+die(status)
